8ffb62e170bf718f43c32871f8447a88c8e2aded
Daniel Collings <collings8915@gmail.com>
2021-04-18 14:55:45 +0100

    Implement starter app structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import { Route, Redirect, Switch } from 'react-router-dom'
+import Spinner from 'react-bootstrap/Spinner'
+import NavBar from './Components/NavBar'
+import About from './Pages/About'
+import Home from './Pages/Home'
+import { fetchCoinData } from './HOCSlices/apiThunk'
+import { useSelector, useDispatch } from 'react-redux'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const dispatch = useDispatch()
+
+  const loadingStatus = useSelector((state) => state.api.status)
+  const error = useSelector((state) => state.api.error)
+
+  useEffect(() => {
+    if (loadingStatus === 'idle') {
+      dispatch(fetchCoinData())
+    }
+  }, [loadingStatus, dispatch])
+
+  if (loadingStatus === 'succeeded') {
+    return (
+      <>
+        <NavBar />
+        <Switch>
+          <Route exact path={'/About'}>
+            <About />
+          </Route>
+          <Route exact path={'/Home'}>
+            <Home />
+          </Route>
+          <Redirect to='/Home' />
+        </Switch>
+      </>
+    )
+  }
+  else if (loadingStatus === 'loading') {
+    return (
+      <Spinner style={{ margin: '50%' }} animation="grow" variant="info" />
+    )
+  } else if (loadingStatus === 'failed') {
+    return (<>{error}</>)
+  }
+  return <></>
+
 }
 
-export default App;
+
+export default App
