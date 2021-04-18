011a2ca61ceac417c881405c1953c346ef064baa
Daniel Collings <collings8915@gmail.com>
2021-04-18 14:55:07 +0100

    Add navbar

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,19 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+const NavBar = () => {
+
+    return (
+        <Navbar collapseOnSelect expand='lg' className='mb-3' bg='dark' variant='dark'>
+            <Navbar.Brand href="#home">RocketDan</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+                <Nav className='mr-auto'>
+                    <Nav.Link href={(document.location.pathname.includes('About') ? '' : `/About`)}>About</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default NavBar
