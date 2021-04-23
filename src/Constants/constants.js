18fe0fe2fa9e54407911c24fb1b761015c6a440a
Daniel Collings <collings8915@gmail.com>
2021-04-23 07:57:02 +0100

    Add constants file

---
+++ src/Constants/constants.js
@@ -0,0 +1,18 @@
+export const PERIODS = [
+    {
+        id: '1d',
+        name: '1d'
+    },
+    {
+        id: '1w',
+        name: '1w'
+    },
+    {
+        id: '1m',
+        name: '1m'
+    },
+    {
+        id: '1y',
+        name: '1y'
+    }
+]
