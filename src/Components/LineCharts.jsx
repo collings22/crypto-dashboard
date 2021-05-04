72e3c733b61aef7b9d4f60c6b3da4c81934b45ee
Daniel Collings <collings8915@gmail.com>
2021-05-04 16:56:52 +0100

    Add multi line chart for comparisons

--- src/Components/LineCharts.jsx
+++ src/Components/LineCharts.jsx
@@ -1,4 +1,5 @@
 import * as d3 from 'd3';
+import { nest } from 'd3-collection'
 import React, { useRef, useEffect } from 'react';
 
 export const SimpleLineChart = (props) => {
@@ -21,13 +22,13 @@ export const SimpleLineChart = (props) => {
 
 
     const drawChart = () => {
-        let data = dataFromProps.map(o => { return {...o, y: Object.values(o).reduce((p,c) => p + (c > 0 ? c : 0), 0) } }) 
+        let data = dataFromProps.map(o => { return { ...o, y: Object.values(o).reduce((p, c) => p + (c > 0 ? c : 0), 0) } })
 
         const svg = d3.select(ref.current);
         let selection = svg.selectAll("rect").data(data);
 
         let y = d3.scaleLinear()
-            .domain([0, Math.max(...data.map(o => o.y))*1.1])
+            .domain([0, Math.max(...data.map(o => o.y)) * 1.1])
             .range([height, 0])
 
         let x = d3.scaleTime()
@@ -77,7 +78,7 @@ export const SimpleLineChart = (props) => {
         var line = svg.selectAll(".lineTest")
             .data([data], function (d) { return new Date(d.label) });
 
-            line = line
+        line = line
             .enter()
             .append("path")
             .attr("class", "lineTest")
@@ -87,9 +88,123 @@ export const SimpleLineChart = (props) => {
             .x(function (d) { return x(new Date(d.label)); })
             .y(function (d) { return y(d.y); });
 
-            line.transition()
+        line.transition()
             .attr("d", valueline)
             .attr('stroke', '#17a2b8')
+            .attr('stroke-width', 1.5)
+            .attr('fill', 'none')
+    }
+
+
+    useEffect(() => {
+        drawChart()
+        // eslint-disable-next-line
+    }, [dataFromProps])
+
+
+    return (
+        <div>
+            <svg ref={ref}></svg>
+        </div>
+    )
+}
+
+
+export const SimpleMultiLineChart = (props) => {
+    const dataFromProps = props.chartData
+    const margin = { top: 10, right: 10, bottom: 0, left: 10 }
+    const width = 400 - margin.top - margin.right
+    const height = 105 - margin.top - margin.bottom
+
+    const ref = useRef()
+
+    useEffect(() => {
+        d3.select(ref.current)
+            .attr('preserveAspectRatio', 'xMinYMin meet')
+            .attr('viewBox', '-25 -15 410 130')
+            .append('g')
+            .attr('transform',
+                'translate(' + margin.left + ',' + margin.top + ')')
+
+    }, [margin.top, margin.left, height, width])
+
+
+    const drawChart = () => {
+        let data = dataFromProps.map(o => { return { ...o, y: Object.values(o).reduce((p, c) => p + (c > 0 ? c : 0), 0) } })
+
+        var sumstat = nest()
+            .key(d => d.type)
+            .entries(data);
+
+        const svg = d3.select(ref.current);
+        let selection = svg.selectAll("rect").data(sumstat);
+
+        let y = d3.scaleLinear()
+            .domain([0, Math.max(...data.map(o => o.y)) * 1.1])
+            .range([height, 0])
+
+        let x = d3.scaleTime()
+            .domain(d3.extent(data, function (d) { return new Date(d.label) }))
+            .range([0, width])
+
+        selection.enter().append('g')
+            .attr('class', 'grid')
+            .style('color', '#17a2b8')
+
+        selection.enter().append('g')
+            .attr('id', 'yAxis')
+            .style('font-size', '50%')
+            .style('color', '#17a2b8')
+            .call(y)
+
+        svg.select("#yAxis")
+            .attr("class", "axis axis--y")
+            .transition()
+            .call(d3.axisLeft(y));
+
+        selection.enter().append('g')
+            .attr('id', 'xAxis')
+            .attr('transform', `translate(0, ${height})`)
+            .style('color', '#17a2b8')
+            .style('font-size', '50%')
+            .call(x)
+            .selectAll('text')
+            .style('text-anchor', 'middle')
+
+        svg.select("#xAxis")
+            .transition()
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        svg.selectAll(".axis.axis--x").transition()
+            .duration(1000)
+            .call(x);
+
+        svg.selectAll(".axis.axis--y").transition()
+            .duration(1000)
+            .call(y);
+
+        svg.selectAll('.lineTest').remove()
+
+        var line = svg.selectAll(".lineTest")
+            .data([data], function (d) { return new Date(d.label) });
+
+
+        line = line
+            .data(sumstat).enter()
+            .append("path")
+            .attr("class", "lineTest")
+            .merge(line);
+
+
+        var valueline = d => d3.line()
+            .x(d => x(new Date(d.label)))
+            .y(d => y(d.y)).curve(d3.curveCardinal)(d.values)
+
+        line.transition().duration(1000)
+            .attr("d", d => valueline(d))
+            .attr('stroke', (d,i) => ['#17a2b8','#94d8e3','#d5ddde'][i])
             .attr('stroke-width', 1.5)
             .attr('fill', 'none')
 
