e4857de32981bc12722046ec497b28af8e5ca808
Daniel Collings <collings8915@gmail.com>
2021-05-06 08:44:17 +0100

    Remove useState data and use redux store selectors

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,179 +6,35 @@ import Button from 'react-bootstrap/Button'
 import Filters from '../Features/Filters'
 import { SimpleLineChart, SimpleMultiLineChart } from '../Components/LineCharts'
 import { SimpleBarChart, SimpleStackedBarChart } from '../Components/BarCharts'
-import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { dispatch } from 'd3-dispatch'
 
 const Home = () => {
-  const [data, setData] = useState([{
-    label: '2021-06-03',
-    y1: 5,
-    y2: 15,
-    y3: 5
+  const dispatch = useDispatch()
+  const handleReduxAction = (actionType, obj) => dispatch({ type: actionType, payload: obj.target.value })
 
-  },
-  {
-    label: '2021-07-04',
-    y1: 5,
-    y2: 5,
-    y3: 5
-  },
-  {
-    label: '2021-08-04',
-    y1: 15,
-    y2: 5,
-    y3: 5
-  },
-  {
-    label: '2021-09-05',
-    y1: 15,
-    y2: 15,
-    y3: 5
-  },
-  {
-    label: '2021-09-06',
-    y1: 5,
-    y2: 5,
-    y3: 5
-  },
-  {
-    label: '2021-10-07',
-    y1: 5,
-    y2: 5,
-    y3: 15
-  },
-  {
-    label: '2021-11-08',
-    y1: 15,
-    y2: 15,
-    y3: 5
-  },
-  {
-    label: '2021-12-09',
-    y1: 5,
-    y2: 5,
-    y3: 15
-  },
-  ])
+  const data = useSelector((state) => state.api.data)
+  const filteredCryptos = useSelector((state) => state.filters.cryptos)
+  const comparableData = data.filter(f => filteredCryptos.includes(f.type))
 
-  const [comparableData, setComparableData] = useState([
-      {
-    label: '2021-07-04',
-    type: 'BTC',
-    y1: 5,
-    y2: 5,
-    y3: 5
-  },
-  {
-    label: '2021-08-04',
-    type: 'BTC',
-    y1: 15,
-    y2: 5,
-    y3: 5
-  },
-  {
-    label: '2021-09-05',
-    type: 'BTC',
-    y1: 5,
-    y2: 15,
-    y3: 5
-  },
-  {
-    label: '2021-09-06',
-    type: 'BTC',
-    y1: 5,
-    y2: 5,
-    y3: 5
-  },
-  {
-    label: '2021-10-07',
-    type: 'BTC',
-    y1: 5,
-    y2: 5,
-    y3: 5
-  },
-  {
-    label: '2021-11-08',
-    type: 'BTC',
-    y1: 5,
-    y2: 5,
-    y3: 5
-  },
-  {
-    label: '2021-12-09',
-    type: 'BTC',
-    y1: 5,
-    y2: 5,
-    y3: 5
-  },
-  {
-    label: '2021-07-04',
-    type: 'ADA',
-    y1: 5,
-    y2: 5,
-    y3: 15
-  },
-  {
-    label: '2021-08-01',
-    type: 'ADA',
-    y1: 15,
-    y2: 15,
-    y3: 5
-  },
-  {
-    label: '2021-09-08',
-    type: 'ADA',
-    y1: 15,
-    y2: 5,
-    y3: 15
-  },
-  {
-    label: '2021-09-15',
-    type: 'ADA',
-    y1: 5,
-    y2: 15,
-    y3: 15
-  },
-  {
-    label: '2021-10-07',
-    type: 'ADA',
-    y1: 5,
-    y2: 5,
-    y3: 5
-  },
-  {
-    label: '2021-11-08',
-    type: 'ADA',
-    y1: 10,
-    y2: 15,
-    y3: 5
-  },
-  {
-    label: '2021-12-09',
-    type: 'ADA',
-    y1: 5,
-    y2: 10,
-    y3: 15
-  }
-  ])
 
   const changeData = () => {
-    setData(data.map(o => { return { ...o, y1: (Math.random() * 15), y2: (Math.random() * 15), y3: (Math.random() * 15) } }))
-    setComparableData(comparableData.map(o => { return { ...o, y1: (Math.random() * 15), y2: (Math.random() * 15), y3: (Math.random() * 15) } }))
+    // setData(data.map(o => { return { ...o, y1: (Math.random() * 15), y2: (Math.random() * 15), y3: (Math.random() * 2000000) } }))
+    // setComparableData(comparableData.map(o => { return { ...o, y1: (Math.random() * 15), y2: (Math.random() * 15), y3: (Math.random() * 2000000) } }))
   }
 
   return (
     <Container fluid>
+      <Button onClick={e => dispatch({type: 'api/randomiseCryptoData', payload: data})}>Randomise Data</Button>
+
       <Card className='mb-3'>
         <Card.Body>
           <Filters />
-          <Button onClick={changeData}>Change Data</Button>
-            Single Charts
           <Row>
-            <Col><SimpleLineChart chartData={data} /></Col>
-            <Col><SimpleStackedBarChart chartData={data} /></Col>
-            <Col><SimpleBarChart chartData={data} /></Col>
+            <Col><SimpleLineChart chartData={comparableData} /></Col>
+            <Col><SimpleStackedBarChart chartData={comparableData} /></Col>
+            <Col><SimpleBarChart chartData={comparableData} /></Col>
           </Row>
-            Comparable Charts
           <Row>
             <Col><SimpleMultiLineChart chartData={comparableData} /></Col>
             <Col><SimpleMultiLineChart chartData={comparableData} /></Col>
