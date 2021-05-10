eb77d8eccf5fdd8e8fdf32d9568a6bee405987df
Daniel Collings <collings8915@gmail.com>
2021-05-10 15:16:37 +0100

    Add profile component using data returned from auth0

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,17 +2,17 @@ import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
 
 const About = () => {
-    document.title = 'About - DC'
-  
-    return (
-      <Container fluid>
-        <Card className='mb-3'>
-          <Card.Body>
-            About Page
-          </Card.Body>
-        </Card>
-      </Container>
-    )
-  }
-  
-  export default About
+  document.title = 'About - DC'
+
+  return (
+    <Container fluid>
+      <Card className='mb-3'>
+        <Card.Body>
+          About Page
+        </Card.Body>
+      </Card>
+    </Container>
+  )
+}
+
+export default About
