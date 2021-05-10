47da6b4aa793e128db416d26105ee50956484cf4
Daniel Collings <collings8915@gmail.com>
2021-05-10 15:13:51 +0100

    Wrap authProvider around the app

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Auth0ProviderWithHistory from './Auth/AuthProvider'
+import reportWebVitals from './reportWebVitals'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <Auth0ProviderWithHistory>
+        <Provider store={store}>
+          <Route component={App} />
+        </Provider>
+      </Auth0ProviderWithHistory>
+
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
