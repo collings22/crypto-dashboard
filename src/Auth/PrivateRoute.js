2cefd2d65bde6a24c86cc7e57c867d3765d83baf
Daniel Collings <collings8915@gmail.com>
2021-05-19 14:48:35 +0100

    Add private route component wrapper

--- src/Auth/PrivateRoute.js
+++ src/Auth/PrivateRoute.js
@@ -4,13 +4,13 @@ import { withAuthenticationRequired } from '@auth0/auth0-react'
 // import { Loading } from './index'
 import Spinner from 'react-bootstrap/Spinner'
 
-
-const PrivateRoute = ({component, ...args}) => (
-    <Route component={withAuthenticationRequired(component, {
-        onRedirecting: () => <Spinner/>,
+const PrivateRoute = ({ component, ...args }) => (
+  <Route
+    component={withAuthenticationRequired(component, {
+      onRedirecting: () => <Spinner />
     })}
     {...args}
-    />
-);
+  />
+)
 
 export default PrivateRoute
